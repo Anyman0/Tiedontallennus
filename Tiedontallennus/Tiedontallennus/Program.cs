883c34d3d443965dc53393c6d2078558c225c26e
Antti Nyman <antti.nyman@stud.pointcollege.fi>
2018-05-07 11:08:07 +0300

    Added code for reading the previously added temperature.

--- Tiedontallennus/Tiedontallennus/Program.cs
+++ Tiedontallennus/Tiedontallennus/Program.cs
@@ -11,13 +11,25 @@ namespace Tiedontallennus
     {
         static void Main(string[] args)
         {
+            string file = @"C:\Users\User\Desktop\Projects\temp.txt";
+
+            // Reading the possible previously added temperature value
+            if (File.Exists(file))
+            {
+                string previousTemp = File.ReadAllText(file);
+                Console.WriteLine("Previously added temperature is: " + previousTemp + " degrees.");
+            }
+            else
+            {
+                Console.WriteLine("Welcome to the temperature application.");
+            }
+
             int temperature;
             Console.Write("Give a new temperature: ");
             string value = Console.ReadLine();
             temperature = int.Parse(value);
 
-            // Saving the given value to a file
-            string file = @"C:\Users\User\Desktop\Projects\temp.txt";
+            // Saving the given value to a file           
             File.WriteAllText(file, temperature.ToString());
             Console.WriteLine("new temperature saved.");
 
