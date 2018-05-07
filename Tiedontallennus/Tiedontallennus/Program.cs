613fc99f1e560090dcc10767a4dfde0ee9be98ef
Antti Nyman <antti.nyman@stud.pointcollege.fi>
2018-05-07 10:57:47 +0300

    Added code for setting and saving temperatures.

--- Tiedontallennus/Tiedontallennus/Program.cs
+++ Tiedontallennus/Tiedontallennus/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Tiedontallennus
 {
@@ -10,6 +11,17 @@ namespace Tiedontallennus
     {
         static void Main(string[] args)
         {
+            int temperature;
+            Console.Write("Give a new temperature: ");
+            string value = Console.ReadLine();
+            temperature = int.Parse(value);
+
+            // Saving the given value to a file
+            string file = @"C:\Users\User\Desktop\Projects\temp.txt";
+            File.WriteAllText(file, temperature.ToString());
+            Console.WriteLine("new temperature saved.");
+
+            Console.ReadLine();
         }
     }
 }
